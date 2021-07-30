1580d1a7774dff5c80c1ddaf6fafaa64b1ba0b59
Ritika <64130468+ritika600@users.noreply.github.com>
2021-07-30 11:24:05 +0530

    Create MirrorNumberPattern.cpp

---
+++ Pattern-2/MirrorNumberPattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin >> n;
+    int i=1;
+    while(i<=n){
+        int k=1;
+        while(k<=n-i){
+            cout << " ";
+            k++;
+        }
+        int j=1;
+        while(j<=i){
+            cout << j;
+        j++;
+        }
+        i++;
+        cout << endl;
+    }
+  
+}
+
+
