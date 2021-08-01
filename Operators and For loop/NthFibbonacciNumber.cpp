87a8f178673ef0d3907e627ad4973351ec6fd884
Ritika <64130468+ritika600@users.noreply.github.com>
2021-08-01 12:32:34 +0530

    Create NthFibbonacciNumber.cpp

---
+++ Operators and For loop/NthFibbonacciNumber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    //Write your code here.  
+    int n;
+    cin >> n;
+    int a=0;
+    int b=1;
+    int c;
+    for(int i=2;i<=n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+        
+        
+    }
+    cout << b;
+    
+}
