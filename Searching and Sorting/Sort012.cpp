e3fa9c2a27d3fcf397b24e1df3931686716fdede
Ritika <64130468+ritika600@users.noreply.github.com>
2021-08-07 23:37:24 +0530

    Create Sort012.cpp

---
+++ Searching and Sorting/Sort012.cpp
@@ -0,0 +1,23 @@
+void sort012(int *arr, int n)
+{
+    //Write your code here
+    int low=0,mid=0,high=n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            int temp=arr[mid];
+            arr[mid]=arr[low];
+            arr[low]=temp;
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1 ){
+            mid++;
+        }
+        else if(arr[mid]==2){
+            int temp=arr[mid];
+            arr[mid]=arr[high];
+            arr[high]=temp;
+            high--;
+        }
+    }
+}
